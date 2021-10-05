4e57e9b62a2339e60e3a708dac343ab42d08735b
JannikEmmerich <mail@jannik-emmerich.de>
2021-10-05 20:42:26 +0200

    Updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fachschaft.physik.kit.edu/ophasen-anmelder/api'
+  apiUrl: 'https://fachschaft.physik.kit.edu/ophase-anmeldung/api'
 };
