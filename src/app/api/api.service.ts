dcfd340996dc788cccddf61c21655d7f6ee557d8
Alexander Kaschta <alexander.kaschta@t-online.de>
2024-09-06 23:29:20 +0200

    UPD: Migrate to Angular 18

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import { Event, EventDraw, EventParticipation, EventType } from "./api.domain";
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {environment} from '../../environments/environment';
 
 @Injectable({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,16 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {HeaderComponent} from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 }
